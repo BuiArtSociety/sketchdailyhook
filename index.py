e8131e069db88296bbe784a4cc1162daf30ed5d2
Paws <lyric_wolf@outlook.com>
2018-11-06 00:30:14 +0000

    Prepping db for pulling

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 import time
 import datetime
+import asyncpg
+import asyncio
 from discord_webhook import DiscordWebhook
 from tools import default
 
@@ -7,10 +9,19 @@ config = default.get("config.json")
 
 print(f"{config.name} by lyricalpaws running!")
 
+
+async def run():
+    credentials = {"user": config.dbname, "password": config.dbpass, "database": config.database, "host": "127.0.0.1"}
+    db = await asyncpg.create_pool(**credentials)
+    await db.execute("CREATE TABLE IF NOT EXISTS sketchdaily(artist varchar, idea varchar);")
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
+
 while True:
     now = datetime.datetime.utcnow()
 
-    if now.hour is 22 and now.minute is 41:
+    if now.hour is 00 and now.minute is 29:
         print('True')
         webhook = DiscordWebhook(url=f'{config.webhookurl}', content='Webhook Message')
         webhook.execute()
