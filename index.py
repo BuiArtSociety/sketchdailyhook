1048498cd8c852ad67f7b8169066b7f767a9a9e1
Paws <lyric_wolf@outlook.com>
2018-11-21 22:09:04 +0000

    This is ok

--- index.py
+++ index.py
@@ -1,37 +1,60 @@
-import time
-import datetime
-import asyncpg
+# Imports
 import asyncio
-from discord_webhook import DiscordWebhook
+import asyncpg
+
+import datetime
+import time
+
+from dhooks import Webhook
+
 from tools import default
 
+# Getting the config file for webhook links
 config = default.get("config.json")
 
+# Print that it's working
 print(f"{config.name} by {config.owner} running!")
 
 
+# Making a function for starting
 async def run():
+    # Naming the database and logging in
     credentials = {"user": config.dbname, "password": config.dbpass, "database": config.database, "host": "127.0.0.1"}
     db = await asyncpg.create_pool(**credentials)
+    # Make the table if it's not here
     await db.execute("CREATE TABLE IF NOT EXISTS sketchdaily(code int, artist varchar, idea varchar);")
-
+    # Cgecj fir the time
     while True:
         now = datetime.datetime.utcnow()
-        dayandmonth = datetime.date.today()
+        dayMonth = datetime.date.today()
+        # If the time is 3 PM UTC and if it is
         if now.hour is 15 and now.minute is 00:
+            # Get a random entry
             row = await db.fetchrow("SELECT * FROM sketchdaily ORDER BY RANDOM() LIMIT 1;")
             if row is None:
+                # If there are no entries print so
                 return print("There are no suggestions...")
+            # Else, say it's ready and start the process
             print('True, sending webhook message')
-            webhook = DiscordWebhook(url=f'{config.webhookurl}', content=f"<@&509164409604669450>\n\nThe prompt for {dayandmonth.day}/{dayandmonth.month}/{dayandmonth.year} is:\n\n**{row['idea']}**\n\nIt was suggested by **{row['artist']}**\n\nPlease post your submission below this line!\n\n===================")
-            webhook.execute()
+            # Define the webhook
+            hook = Webhook(config.webhookurl, is_async=True)
+            hook2 = Webhook(config.webhook2, is_async=True)
+            # Send webhook messages to the two servers
+            await hook.send(f"<@&509164409604669450>\n\nThe prompt for {dayMonth.day}/{dayMonth.month}/{dayMonth.year} is:\n\n**{row['idea']}**\n\nIt was suggested by **{row['artist']}**\n\nPlease post your submission below this line!\n\n===================")
+            await hook2.send(f"Want some inspiration to draw or write?\nToday's prompt is:\n\n**{row['idea']}**\nSuggested by **{row['artist']}**\n\nTry out making something using this!!\nPost your submissions in this channel when done!")
+            # End it
+            await hook.close()
+            await hook2.close()
+            # FInd the entry, delete and sleep for a minuite
             sketchcode = row['code']
             query = "DELETE FROM sketchdaily WHERE code=$1;"
             await db.execute(query, sketchcode)
             time.sleep(60)
         else:
+            # Else, check again in 15 seconds
             time.sleep(15)
 
 
+# The async loop begins
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
